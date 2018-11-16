f45f5e27ea7099ddab463e9b581e960461a6b1d6
TCXX <tiancai_xinxin@126.com>
2018-11-15 20:41:09 -0500

    Two-class system constant parameters
    The third part of the utility function is based on the number of agents in each class; need to verify

--- Income Inequality/PayGame.py
+++ Income Inequality/PayGame.py
@@ -14,43 +14,43 @@ import random
 import math
 
 
-#count_levels_combined = []
-num_levels = 100
-count_levels_list = np.zeros((100, 1))
-num_agents = 10000
-agent_levels_list = np.zeros((10000, 1)) # which level the agent is at
-agent_alpha_list = np.zeros(10000)
-agent_beta_list = np.zeros(10000)
-agent_gamma_list = np.zeros(10000)
-#count_num_agents_old = 10000
-#index_list = [0, 1, 2, 3, 4]
-#switch_list = []
-N_list = [1.0, 0, 0, 0, 0]
+
+
+#agent_alpha_list = np.zeros(10000)
+#agent_beta_list = np.zeros(10000)
+#agent_gamma_list = np.zeros(10000)
+
+N_list = [0.5, 0.5, 0, 0, 0]
 alpha_list = [93.4, 95.8, 100, 100, 100]
 beta_list = [3.87, 3.67, 4, 4, 4]
 gamma_list = [2.17, 4.34, 5, 5, 5]
 
+num_levels = 100
+count_levels_list = np.zeros((100, 5)) # number of agents for given level and class
+num_agents = 10000
+agent_levels_list = np.zeros(10000) # which level the agent is at
+num_classes = 5
+agent_classes_list = np.random.choice(5, 10000, p=N_list)
+
 s_min = 20000.0
 s_max = 3000000.0
-#turtle_count = 0
 
-epoch = 100
+epoch = 25
 
 def level_to_salary(x):
     return s_min + (s_max - s_min) / (num_levels - 1) * (x - 1)
 
 def setup():
-    #random.seed()
     mean = num_levels / 2
     for i in range(num_agents):
-        #r = random.randint(0, num_levels - 1) # a random level
-        r = int(round(mean))
-        count_levels_list[r, 0] += 1
-        agent_levels_list[i, 0] = r
+        r = int(round(mean)) # all agents start at the mean level
+        c = agent_classes_list[i]
+        count_levels_list[r, c] += 1
+        agent_levels_list[i] = r
     
-    agent_alpha_list[:] = np.random.normal(alpha_list[0], 3, 10000)
-    agent_beta_list[:] = np.random.normal(beta_list[0], 0.3, 10000)
-    agent_gamma_list[:] = np.random.normal(beta_list[0], 0.3, 10000)
+    #agent_alpha_list[:] = np.random.normal(alpha_list[0], 0.2, 10000)
+    #agent_beta_list[:] = np.random.normal(beta_list[0], 0.02, 10000)
+    #agent_gamma_list[:] = np.random.normal(beta_list[0], 0.02, 10000)
     
 
 def turtle():
@@ -58,16 +58,19 @@ def turtle():
     
     for i in range(num_agents):
         r = random.randint(0, num_levels - 1)
+        c = agent_classes_list[i]
+        
         level_target = r
-        level_self = int(round(agent_levels_list[i, 0]))
+        level_self = int(round(agent_levels_list[i]))
         
         s_target = level_to_salary(level_target + 1)
         s_self = level_to_salary(level_self + 1)
         
-        num_target = count_levels_list[level_target, 0]
-        num_self = count_levels_list[level_self, 0]
+        num_target = count_levels_list[level_target, c]
+        num_self = count_levels_list[level_self, c]
         
-        alpha, beta, gamma = agent_alpha_list[i], agent_beta_list[i], agent_gamma_list[i]
+        #alpha, beta, gamma = agent_alpha_list[i], agent_beta_list[i], agent_gamma_list[i]
+        alpha, beta, gamma = alpha_list[c], beta_list[c], gamma_list[c]
         
         payoff_target = alpha * math.log(s_target) 
         payoff_target -= beta * math.log(s_target) ** 2
@@ -80,21 +83,19 @@ def turtle():
         
         if payoff_target > payoff_self:
             # move from self to target
-            count_levels_list_copy[level_self, 0] -= 1
-            count_levels_list_copy[level_target, 0] += 1
-            agent_levels_list[i, 0] = level_target
+            count_levels_list_copy[level_self, c] -= 1
+            count_levels_list_copy[level_target, c] += 1
+            agent_levels_list[i] = level_target
     
     count_levels_list[:, :] = count_levels_list_copy[:, :]
     
 def plot():
-    positions = np.arange(num_levels)
-    plt.bar(positions, count_levels_list, 1.0)
-    plt.xticks(positions + 1.0 / 2, ('0', '1', '2', '3'))
-    plt.show()
+    x = np.linspace(0, num_levels, num_levels)
+    plt.plot(x, count_levels_list[:, 0], label="class 1", marker='', color='skyblue')
+    plt.plot(x, count_levels_list[:, 1], label="class 2", marker='', color='olive')
 
 if __name__ == '__main__':
     setup()
     for i in range(epoch):
-        if i % 10 == 0 or i < 10 or i + 1 == epoch:
-            plot()
         turtle()
+    plot()
