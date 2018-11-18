bf2e868d0adaa1f7bd3aa655ecb794027cf8b7d3
agango <ag3661@columbia.edu>
2018-11-18 03:13:01 -0500

    initial implementation using mnist network, less weights and less complex network

--- Understanding Deep Learning/wang_landau.py
+++ Understanding Deep Learning/wang_landau.py
@@ -1,42 +1,104 @@
-g_map = {}
-histogram = {}
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.utils import np_utils
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+
+# define baseline model
+def baseline_model():
+    # create model
+    model = Sequential()
+    l1 = Dense(12, input_dim=num_pixels, kernel_initializer='uniform', activation='relu')
+    l2 = Dense(num_classes, kernel_initializer='normal', activation='softmax')
+    l1 = model.add(l1)
+    l2 = model.add(l2)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model, l1, l2
+
+def set_weights(weights, l1, l2):
+    
+
+# build the model
+model, l1, l2 = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+#number of bins = 100 
+#energy is defined as cross entropy over entire training set 
+g_map = {} #map from bin endpoints to densities
+histogram = {} #map from bin endpoints to counts
+histogram_map = {} #map from weights to bin endpoints
+
+min_energy = 0
+max_energy = log(100) #upper bound - 
+#if the network predicts a probability of .01 for the correct label for every sample, then the energy will be num_samples times this amount
+flatness = 0.9
 
 def cost_function(weights):
-    cur_cost = 0    
+    set_weights(weights)
 
-def g_function(weights):
-    if weights in g_map:
-        return g_map[weights]
-    else:
-        return 1
+
+def g_function(energy):
+    if energy not in g_map:
+        g_map[energy] = 1
     
-def update_g_function(weights, alpha):
-    if weights in g_map:
-        g_map[weights] = g_map[weights]*alpha
+    return g_map[energy]
+    
+def update_g_function(energy, alpha):
+    if energy in g_map:
+        g_map[energy] = g_map[energy]*alpha
 
-def update_histogram(weights):
-    if weights in histogram:
-        histogram[weights] += 1
+def update_histogram(energy):
+    if energy in histogram:
+        histogram[energy] += 1
     else:
-        histogram[weights] = 1
+        histogram[energy] = 1
 
 def wang_landau(alpha, epsilon):
-    x_i = cost_function(np.rand()) # a random initial configuration
+    x_i = np.random.normal(size = 784*12 + 12*num_classes) # a random initial configuration
     currentEnergy = cost_function(x_i)
     
     while (alpha - 1 > epsilon):
-        x_propose = np.random()  
-        proposedEnergy = cost_function(x_propose) # the energy of the proposed configuration computed
-       
-        p_i = 1/(g_function(currentEnergy)*1.0)
-        p_propose = 1/(g_function(proposedEnergy)*1.0)
-        p = min(1, p_propose/p_i)
-        
-        indicator = np.random.binomial(1, p)
-        if indicator == 1:
-            x_i = x_propose
-            currentEnergy = proposedEnergy
-        
-        update_histogram(x_i)
-        update_g_function(x_i, alpha)
+        while True:
+            x_propose = np.random()  
+            proposedEnergy = cost_function(x_propose) # the energy of the proposed configuration computed
+           
+            p_i = 1/(g_function(currentEnergy)*1.0)
+            p_propose = 1/(g_function(proposedEnergy)*1.0)
+            p = min(1, p_propose/p_i)
+            
+            indicator = np.random.binomial(1, p)
+            if indicator == 1:
+                x_i = x_propose
+                currentEnergy = proposedEnergy
+            
+            update_histogram(currentEnergy)
+            update_g_function(currentEnergy, alpha)
+
+
         
